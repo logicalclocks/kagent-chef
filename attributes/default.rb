eb90e0e1264e3571a36e2e0d4b035b3494c302cf
karamel <jdowling@sics.se>
2018-06-14 23:57:38 +0200

    move attrs from hopsworks version here

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,10 +4,11 @@ default["install"]["ssl"]                          = "false"
 default["install"]["cleanup_downloads"]            = "false"
 default["install"]["upgrade"]                      = "false"
 default["install"]["addhost"]                      = "false"
-
+default["install"]["version"]                      = "0.6.0-SNAPSHOT"
+default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0"
 
 # Default values for configuration parameters
-default["kagent"]["version"]                       = "0.2.0"
+default["kagent"]["version"]                       = node["install"]["version"]
 default["kagent"]["user"]                          = node["install"]["user"].empty? ? "kagent" : node["install"]["user"]
 default["kagent"]["group"]                         = node["install"]["user"].empty? ? "kagent" : node["install"]["user"]
 default["kagent"]["certs_group"]                   = "certs"
