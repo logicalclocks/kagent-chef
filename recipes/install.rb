80dc226c83c228357a43ac30c62157ab729bc1bb
Jim Dowling <jim@logicalclocks.com>
2022-04-18 07:42:55 +0200

    install acl package for ubuntu-20.04

--- recipes/install.rb
+++ recipes/install.rb
@@ -21,7 +21,7 @@ end
 
 case node["platform_family"]
 when "debian"
-  package ["build-essential", "libssl-dev", "jq"]
+  package ["build-essential", "libssl-dev", "jq", "acl"]
 
 # Change lograte policy
   cookbook_file '/etc/logrotate.d/rsyslog' do
