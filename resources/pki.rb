fe81ddb9fe8aa98e69f0f930d35e869e55548197
Antonis Kouzoupis <antonios@logicalclocks.com>
2022-09-23 16:08:51 +0200

    [HOPSWORKS-3165] Infrastructure code for replacing openssl CA with BouncyCastle (#200)
    * [HOPSWORKS-3165] Add provider to sign CSR
    
    [HOPSWORKS-3165] Configurable CA path
    
    * [HOPSWORKS-3165] Fix variable name
    
    * [HOPSWORKS-3165] Add property to override http port

---
+++ resources/pki.rb
@@ -0,0 +1,6 @@
+actions :sign_csr
+
+attribute :csr_file, :kind_of => String
+attribute :output_dir, :kind_of => String
+attribute :ca_path, :kind_of => String
+attribute :http_port, :kind_of => Integer
