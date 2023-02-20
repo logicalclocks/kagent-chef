19718440d8e8fd767ef7d0f50b77ed3fdc97368a
Ermias <ermiasg@kth.se>
2023-02-20 14:46:25 +0100

    use recipe ip

--- resources/pki.rb
+++ resources/pki.rb
@@ -4,4 +4,3 @@ attribute :csr_file, :kind_of => String
 attribute :output_dir, :kind_of => String
 attribute :ca_path, :kind_of => String
 attribute :http_port, :kind_of => Integer
-attribute :host, :kind_of => String, :default => "127.0.0.1"
