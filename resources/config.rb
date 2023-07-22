b06e441a57686d7d4ce81471f3e42916a6c9c3ff
Fabio Buso <fabio@hopsworks.ai>
2023-07-22 20:11:14 +0200

    [HWORKS-647] Replace Chef with Cinc (#211)
    * [HOPSWORKS-2321] Migrate from Chef to cinc
    
    * Fix location of chef certificates

--- resources/config.rb
+++ resources/config.rb
@@ -2,8 +2,8 @@ actions :add, :systemd_reload, :remove
 
 attribute :services_file, :kind_of => String, :default => node["kagent"]["services"]
 attribute :role, :kind_of => String, :name_attribute => true, :required => true
-attribute :service, :kind_of => String, :required => true
-attribute :log_file, :kind_of => String 
+attribute :service, :kind_of => String
+attribute :log_file, :kind_of => String
 attribute :config_file, :kind_of => String, :default => ""
 attribute :fail_attempts, :kind_of => Integer, :default => 1
 attribute :command, :kind_of => String, :default => ""
--- resources/keys.rb
+++ resources/keys.rb
@@ -1,8 +1,8 @@
 actions :generate, :return_publickey, :get_publickey, :append2ChefTrustAnchors
 
-attribute :homedir, :kind_of => String, :name_attribute => true, :required => true
+attribute :homedir, :kind_of => String, :name_attribute => true
 attribute :cb_name, :kind_of => String
 attribute :cb_recipe, :kind_of => String
-attribute :cb_user, :kind_of => String, :required => true
-attribute :cb_group, :kind_of => String, :required => true
+attribute :cb_user, :kind_of => String
+attribute :cb_group, :kind_of => String
 attribute :hopsworks_alt_url, :kind_of => String, default: nil
